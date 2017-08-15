43b1d4728421b4c8bd55668edc8deead72c92d59
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2017-08-15 14:51:19 +0100

    Add methods to APplication Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def check_for_mobile
+    session[:mobile_override] = params[:mobile] if params[:mobile]
+  end
+
+  def mobile_device?
+    if session[:mobile_override]
+      session[:mobile_override] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
 end
+
+helper_method :mobile_device?
