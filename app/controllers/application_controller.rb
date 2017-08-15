bf63bae93ea65497495efcd3df4d86e2d4f45a8b
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2017-08-15 15:00:39 +0100

    Complete mobile views adition

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def check_for_mobile
-    session[:mobile_override] = params[:mobile] if params[:mobile]
-  end
+  before_filter :check_for_mobile
 
   def mobile_device?
     if session[:mobile_override]
@@ -11,6 +8,15 @@ class ApplicationController < ActionController::Base
     else
       request.user_agent =~ /Mobile|webOS/
     end
+  end
+
+  def prepare_for_mobile
+    prepend_view_path Rails.root + 'app' + 'mobile_views'
+  end
+
+  def check_for_mobile
+    session[:mobile_override] = params[:mobile] if params[:mobile]
+    prepare_for_mobile if mobile_device?
   end
 end
 
