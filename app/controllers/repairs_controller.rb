28831e41bb95fcc34d1ecc3c35ae6ad9092a0fa5
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2017-08-17 16:07:11 +0100

    Add prelim code to Repairs controller

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -1,7 +1,15 @@
 class RepairsController < ApplicationController
+  before_action :set_repair, only: [:show]
+  
   def new
+    @repair = Repair.new
   end
 
   def show
   end
+
+  private
+    def set_repair
+      @repair = Repair.find(params[:id])
+    end
 end
