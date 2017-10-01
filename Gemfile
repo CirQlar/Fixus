100a56fe354969437894a7fc2aed0c5b8a3727e0
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2017-10-01 22:05:29 +0100

    Style cva

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'haml-rails', '~> 1.0'
 gem 'simple_form'
 gem 'devise'
+gem 'will_paginate', '~> 3.1.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def repair_status_date(repair)
+    case repair.status
+    when "awaiting_pick_up"
+      date = "";
+    when "picked_up"
+      date = repair.pick_up_time.to_date.to_s
+    when "fixed"
+      date = repair.fix_time.to_date.to_s
+    when "delivered"
+      date = repair.deliver_time.to_date.to_s
+    when "cancelled"
+      date = repair.cancel_time.to_date.to_s
+    end
+
+    return date
+  end
 end
