35851567594cc123a2796758a03a31252b2417fa
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2017-10-29 16:21:32 +0100

    Style dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def dashboard
+    @repair = Repair.last
+    @device = @repair.device
+    @address = @repair.address
   end
 
 end
