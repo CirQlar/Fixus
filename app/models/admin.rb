1cc1045096146279db1a29ce6e8820345a07c011
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2017-12-26 20:01:48 +0100

    Add validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,16 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  VALID_PHONE_REGEX = /^(\+[1-9][0-9]*(\([0-9]*\)|-[0-9]*-))?[0]?[1-9][0-9\- ]*$/i
+  validates :phone, presence: true, format: { with: VALID_PHONE_REGEX }
+  validates :firstname, presence: true, length: {maximum: 255}
+  validates :lastname, presence: true, length: {maximum: 255}
+  validates :dob, presence:true
+  validates :gender, presence: true
+
+  enum gender: {
+    male: 0,
+    female: 1
+  }
 end
