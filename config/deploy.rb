7ffd2ec93ae3cf8920b206c4921b0cfa9c781cd5
Ayanfe <ibitoyeayanfeoluwa@yahoo.com>
2018-01-15 17:08:41 +0100

    sjdw

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,8 +92,7 @@ namespace :deploy do
     on primary fetch(:migration_role) do
       within release_path do
         with rails_env: fetch(:rails_env) do
-          execute "export DISABLE_DATABASE_ENVIRONMENT_CHECK=1"
-          execute :rake, "db:migrate:reset"
+          execute :rake, "db:reset"
         end
       end
     end
